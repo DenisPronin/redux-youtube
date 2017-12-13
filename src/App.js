671db7a57cdf9dc672a530314d5111cf5f329e53
Denis <pronin.idenis@gmail.com>
2017-12-13 21:57:49 +0700

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import store from './redux/store';
 import './App.css';
 import SearchView from './views/SearchView'
 
 class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <SearchView />
-      </div>
+      <Provider store={store} >
+        <div className='app'>
+          <SearchView />
+        </div>
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-import store from './redux/store';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store} >
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
 
